f8fbfa3098e5f38c14f7eee75d799d7093b49aee
Teitei Wisdom <120546016+Ajinormotor@users.noreply.github.com>
2023-09-18 06:18:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var navLink = document.getElementById('navLink');
+
+function showMenu()  {
+    navLink.style.left= "0px";
+}
+
+
+function hideMenu()  {
+    navLink.style.left= "-300px";
+    
+}
+
+
